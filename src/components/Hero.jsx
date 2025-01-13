72d73368fb67d01b73418373ebd623ce77b0d64e
Akshay <erakshayjadhav8@gmail.com>
2025-01-13 16:43:28 +0530

    animated title component done

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,6 +1,12 @@
-import React, { useRef, useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import Button from "./Button";
 import { TiLocationArrow } from "react-icons/ti";
+import { useGSAP } from "@gsap/react";
+import gsap from "gsap";
+
+import { ScrollTrigger } from "gsap/all";
+
+gsap.registerPlugin(ScrollTrigger);
 
 const Hero = () => {
   const [currentIndex, setCurrentIndex] = useState(1);
@@ -33,8 +39,75 @@ const Hero = () => {
     return `videos/hero-${index}.mp4`;
   };
 
+
+  useEffect(() => {
+    if (loadedVideos === totalVideos - 1) {
+      setIsLoading(false);
+    }
+  }, [loadedVideos])
+  
+
+
+  // gsap for zooming video animation
+  useGSAP(
+    () => {
+      if (hasClicked) {
+        gsap.to("#next-video", { visibility: "visible" });
+
+        gsap.to("#next-video", {
+          transformOrigin: "center center",
+          scale: 1,
+          width: "100%",
+          height: "100%",
+          duration: 1,
+          ease: "power1.inOut",
+          onStart: () => nextVideoRef.current.play(),
+        });
+
+        gsap.from("#current-video", {
+          transformOrigin: "center center",
+          scale: 0,
+          duration: 1.5,
+          ease: "power1.inOut",
+        });
+      }
+    },
+    { dependencies: [currentIndex], revertOnUpdate: true }
+  );
+
+
+  // gsap for scroll effect
+  useGSAP(() => {
+    gsap.set("#video-frame", {
+      clipPath: "polygon(14% 0%, 72% 0%, 90% 90%, 0% 100%)",
+      borderRadius: "0 0 40% 10%",
+    });
+
+    gsap.from("#video-frame", {
+      clipPath: "polygon(0% 0%, 100% 0%, 100% 100%, 0% 100%)",
+      borderRadius: "0 0 0 0",
+      ease: "power1.inOut",
+      scrollTrigger: {
+        trigger: "#video-frame",
+        start: "center center",
+        end: "bottom center",
+        scrub: "true",
+      },
+    });
+  });
+
   return (
     <div className="relative h-dvh w-screen overflow-x-hidden">
+      {isLoading && (
+        <div className="flex-center absolute z-[100] h-dvh w-screen overflow-hidden bg-violet-50">
+          <div className="three-body">
+            <div className="three-body__dot" />
+            <div className="three-body__dot" />
+            <div className="three-body__dot" />
+          </div>
+        </div>
+      )}
+
       <div
         id="video-frame"
         className="relative z-10 h-dvh w-screen overflow-hidden rounded-lg bg-blue-75"
@@ -74,7 +147,7 @@ const Hero = () => {
               src={getVideoSrc(
                 currentIndex === totalVideos - 1 ? 1 : currentIndex
               )}
-            //   autoPlay
+              // autoPlay
               loop
               muted
               className="absolute left-0 top-0 size-full object-cover object-center"
@@ -94,11 +167,19 @@ const Hero = () => {
                 Enter the Metagame Layer <br />
                 Unleash the Play Economy
               </p>
-              <Button id="watch-trailer" title="Watch Trailer" leftIcon={<TiLocationArrow/>} containerClass='bg-' />
+              <Button
+                id="watch-trailer"
+                title="Watch Trailer"
+                leftIcon={<TiLocationArrow />}
+                containerClass="!bg-yellow-300 flex-center gap-1"
+              />
             </div>
           </div>
         </div>
       </div>
+      <h1 className="special-font hero-heading absolute bottom-5 right-5  text-black">
+        G<b>a</b>ming
+      </h1>
     </div>
   );
 };
