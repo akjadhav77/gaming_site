99f7064ce0367974e48cb788d912e529b1747bfb
Akshay <erakshayjadhav8@gmail.com>
2025-01-15 17:49:22 +0530

    footer done

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,9 +1,32 @@
-import React from 'react'
+import React from "react";
+import { FaDiscord, FaFacebook, FaGithub, FaInstagram, FaTwitter } from "react-icons/fa";
 
+const links = [
+  { icon: <FaFacebook/>, href: "https://www.facebook.com" },
+  { icon: <FaDiscord/>, href: "https://www.discord.com" },
+  { icon: <FaGithub/>, href: "https://www.github.com" },
+  { icon: <FaInstagram/>, href: "https://www.instagram.com" },
+  { icon: <FaTwitter/>, href: "https://www.twitter.com" },
+];
 const Footer = () => {
   return (
-    <div>Footer</div>
-  )
-}
+    <div className="w-screen bg-violet-300 py-4 text-black ">
+      <div className="container mx-auto flex flex-col items-center justify-between gap-4 px-4 md:flex-row">
+        <p className="text-center text-sm font-normal md:text-left">
+          &copy; AK 2025. All Rights Reserved.
+        </p>
 
-export default Footer
+        <div className="flex justify-center gap-4 md:justify-start">
+          {links.map((link, index) => (
+            <a key={index} href={link.href} target="_blank" rel="noopener noreferrer" className="text-black transition-colors duration-500 ease-in-out hover:text-white">{link.icon}</a>
+          ))}
+        </div>
+        <a href="#privacy-policy" className="text-center text-sm hover:underline md:text-right">
+          Privacy Policy
+        </a>
+      </div>
+    </div>
+  );
+};
+
+export default Footer;
